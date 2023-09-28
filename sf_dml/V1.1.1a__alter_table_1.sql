e409dcb8be99612c5e14bbdd1a1bd18e088b4410
jagadeesch1981 <jagadeesch1981@gmail.com>
2023-09-27 20:36:47 -0400

    added alter statement

--- sf_dml/V1.1.1a__alter_table_1.sql
+++ sf_dml/V1.1.1a__alter_table_1.sql
@@ -1,10 +1,3 @@
 USE SCHEMA TEMP_SCHEMA;
 
-IF NOT EXISTS (
-    SELECT 1 
-    FROM information_schema.columns 
-    WHERE table_name = 'hello_word' AND column_name = 'middle_name'
-)
-BEGIN
-    ALTER TABLE hello_word ADD COLUMN middle_name VARCHAR;
-END;
+ALTER TABLE hello_word ADD COLUMN middle_name VARCHAR;
